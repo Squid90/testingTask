b1afe71a8e8073ccd6c47113760d2774579e9365
Squid90 <squid90@rambler.ru>
2023-12-09 06:44:28 +0300

    u20

--- src/js/app.js
+++ src/js/app.js
@@ -1,14 +1,18 @@
 // TODO: write code here
 let currentScore = 0;
 let failScore = 0;
-let activeHole;
+let activeHole = Math.floor(1 + Math.random() * 16);
 let playing = true;
+let cycleNumber = 0;
 const hole = document.querySelectorAll('.hole');
 const holeSeparate = [...hole];
 const deactivateHole = () => document.querySelector('.hole_has-goblin').classList.remove('hole_has-goblin');
 const activateHole = (index) => document.querySelector(`.hole${index}`).classList.add('hole_has-goblin');
+//activateHole(activeHole);
+
 
 function changeHole() {
+  activateHole(activeHole);
   deactivateHole();
   activeHole = Math.floor(1 + Math.random() * 16);
   activateHole(activeHole);
@@ -16,19 +20,27 @@ function changeHole() {
 function reset() {
   alert('Игра окончена!');
   currentScore = 0;
-  failScore = 0;
+  failScore = -1;
   playing = true;
   document.querySelector('.success').textContent = 0;
   document.querySelector('.fail').textContent = 0;
+  cycleNumber = -1;
 }
 (() => {
+  
   if (playing) {
     setInterval(() => {
-      if (!playing) {
+      if (cycleNumber !== currentScore + failScore) {
+        failScore += 1;
+        document.querySelector('.fail').textContent = failScore;
+      }
+
+      if (!playing || failScore >= 5) {
         reset();
         return;
       }
       changeHole();
+      cycleNumber += 1;
     }, 1000);
   }
 })();
@@ -43,9 +55,6 @@ holeSeparate.forEach((item) => {
       changeHole();
       failScore += 1;
       document.querySelector('.fail').textContent = failScore;
-      if (failScore === 5) {
-        playing = false;
-      }
     }
   });
 });
